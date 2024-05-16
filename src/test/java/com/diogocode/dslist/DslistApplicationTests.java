ddfae75180085f24f7b7863ce70df8015db45149
diogobarroscode <diogobarros.code@gmail.com>
2024-05-16 01:10:54 -0300

    Criação do projeto java spring

---
+++ src/test/java/com/diogocode/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogocode.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
